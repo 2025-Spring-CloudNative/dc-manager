e842179b99dd10fe29973a05767513a350563838
eric891224 <s96006730@gmail.com>
2025-05-02 15:18:33 +0800

    add tanstack-query, axios; add basic getDataCenters hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.scss';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.scss"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <RouterProvider router={router} />
-  </StrictMode>
-);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { RouterProvider } from "react-router-dom"
+import router from "./router"
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 0,
+        },
+    },
+})
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </StrictMode>
+)
