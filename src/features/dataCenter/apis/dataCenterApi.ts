08d47d28a579b6e121a68708f35cdb21305e4590
eric891224 <s96006730@gmail.com>
2025-05-07 21:20:14 +0800

    add AuthProvider

--- src/features/dataCenter/apis/dataCenterApi.ts
+++ src/features/dataCenter/apis/dataCenterApi.ts
@@ -1,15 +1,8 @@
-import axios from "axios"
-
-const apiInstance = axios.create({
-    baseURL: "http://localhost:4000/data-center",
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+import api from "@lib/axios"
 
 export async function getDataCenters() {
     try {
-        const response = await apiInstance.get("/")
+        const response = await api.get("/data-center")
         return response.data
     } catch (error) {
         console.error("Error fetching data center data:", error)
