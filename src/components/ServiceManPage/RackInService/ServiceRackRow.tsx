63704789b96b7bc8fa31048865ad98ef6aa66303
梁詠晴 <carrieliang.tw@gmail.com>
2025-05-24 15:09:58 +0800

    update rack utilization

--- src/components/ServiceManPage/RackInService/ServiceRackRow.tsx
+++ src/components/ServiceManPage/RackInService/ServiceRackRow.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './ServiceRackTable.module.scss';
 
-export default function ServiceRackRow({rack, onToggle, isExpanded}) {
+export default function ServiceRackRow({rack, onToggle, isExpanded, rackUtilization}) {
+
+  console.log("Rack Utilizations:", rackUtilization.utilization);
   return (
     <div className={styles.row}>
       <button
@@ -11,9 +13,9 @@ export default function ServiceRackRow({rack, onToggle, isExpanded}) {
       <div className={styles.tableRowText} style={{ width: '135px' }}>{rack.name}</div>
       <div className={styles.sepLine}></div>
       <div className={styles.utilBar}>
-        <div style={{ width: `${(rack.utilization / 100) * 150}px` }} 
-        className={`${styles.utilFill} ${rack.utilization >= 80 ? styles.utilHigh : ''}`} />
-        <span>{rack.utilization}%</span>
+        <div style={{ width: `${(rackUtilization.utilization) * 150}px` }} 
+        className={`${styles.utilFill} ${rackUtilization.utilization >= 0.8 ? styles.utilHigh : ''}`} />
+        <span>{rackUtilization.utilization}%</span>
       </div>
     </div>
   );
--- src/components/ServiceManPage/RackInService/ServiceRackTable.tsx
+++ src/components/ServiceManPage/RackInService/ServiceRackTable.tsx
@@ -22,6 +22,7 @@ interface ServiceRackTableProps {
   selectedServiceRack: Service;
 }
 // todo: get rack using service id
+/*
 const initialRack = [
   { id: 1, name: 'Rack A',  utilization: 35 },
   { id: 2, name: 'Rack B', utilization: 45 },
@@ -32,14 +33,14 @@ const initialRack = [
 const initialMachine= [
   { id: 1, rackId: 1, ip: "192.168.0.1",  dc:"dc a", rack:"rackkk", room: "room a", name: "host A", status: "active", startUnit: 1, unit: 3},
   { id: 1, rackId: 4,  ip: "192.168.0.1", dc:"dc a", rack:"rackkk", room: "room a",  name: "host bbbb", status: "active", startUnit: 5, unit: 2},
-];
+];*/
 
 
 
 
 export default function ServiceRackTable({ selectedServiceRack }: ServiceRackTableProps) {
-  const [rack, setRack] = useState(initialRack);
-  const [machine, setMachine] = useState(initialMachine);
+  //const [rack, setRack] = useState(initialRack);
+  //const [machine, setMachine] = useState(initialMachine);
   const [expandedRackId, setExpandedRackId] = useState<number | null>(null);
   const { data: serviceData, isLoading: isLoadingServices, isError: isErrorServices } = useGetServicesQuery();
   const { data: rackData, isLoading: isLoadingRack, isError: isErrorRack } = useGetRackQuery();
@@ -58,34 +59,52 @@ export default function ServiceRackTable({ selectedServiceRack }: ServiceRackTab
   //const racksByService = groupRacksByService(rackData, serviceData);
   console.log("selected_service", rackInSelectedService);
 
+  const rackUtilizations = rackInSelectedService.map(rack => {
+    const machinesInRack = machineData.filter(machine => machine.rackId === rack.id);
+    const usedUnits = machinesInRack.reduce((total, machine) => total + machine.unit, 0);
+
+    const utilization = rack.height > 0 ? usedUnits / rack.height : 0;
+    return {
+      rackId: rack.id,
+      usedUnits,
+      totalUnits: rack.height,
+      utilization,
+    };
+  });
+
+  
+  console.log("Rack Utilizations:", rackUtilizations);
+
   return (
     <div className={styles.tableContainer}>
       <ServiceRackHeader />
-      {rackInSelectedService.map(rack => (
-        <Fragment key={rack.id}>
-        <ServiceRackRow 
-          key={rack.id} rack={rack} 
-          onToggle={() => toggleRack(rack.id)} 
-          isExpanded={expandedRackId === rack.id}
-        />
-        {expandedRackId === rack.id && (
-          <>
-          {
-            machineData
-              .filter(m => m.rackId === rack.id) 
-              .map(machine => (
-                <ServiceRackMachineRow
-                  key={machine.id}
-                  machine={machine}
-                  rack={rack}
-                />
-              ))
-          }
-        </>
-        )}
-        </Fragment>
-      ))}
-      
+      {rackInSelectedService.map(rack => {
+        const rackUtil = rackUtilizations.find(util => util.rackId === rack.id);
+        console.log("rackUtil", rackUtil);
+        return (
+          <Fragment key={rack.id}>
+            <ServiceRackRow 
+              rack={rack} 
+              onToggle={() => toggleRack(rack.id)} 
+              isExpanded={expandedRackId === rack.id}
+              rackUtilization={rackUtil}
+            />
+            {expandedRackId === rack.id && (
+              <>
+                {machineData
+                  .filter(m => m.rackId === rack.id) 
+                  .map(machine => (
+                    <ServiceRackMachineRow
+                      key={machine.id}
+                      machine={machine}
+                      rack={rack}
+                    />
+                  ))}
+              </>
+            )}
+          </Fragment>
+        );
+      })}
     </div>
-  );
+  )
 }
